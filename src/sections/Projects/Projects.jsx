b91b062228b2052a6bce6c2e5f58f38e87e2ec9d
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2024-07-28 22:14:52 +0530

    Projects Section
    Projects Styling

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "./ProjectsStyles.module.css";
+import Viberr from "../../assets/viberr.png";
+import styles from './ProjectsStyles.module.css';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
-    <div>
-      Projects
-    </div>
-  )
+    <section id="projects" className={styles.container}>
+      <div className="sectionTitle">Projects</div>
+      <div className={styles.projectsContainer}>
+        <div>
+          <a href="https://github.com/m-vishal-10/NewsFeed" target="_blank">
+            <img className="hover" src={Viberr} alt="Viberr" />
+            <h3>NewsFeed</h3>
+            <p>News Articles</p>
+          </a>
+        </div>
+        
+        <div>
+          <a href="https://github.com/m-vishal-10/NewsFeed" target="_blank">
+            <img className="hover" src={Viberr} alt="Viberr" />
+            <h3>Digital Awareness</h3>
+            <p>Digital Awareness App</p>    
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
