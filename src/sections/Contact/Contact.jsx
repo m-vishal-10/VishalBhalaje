ccb2f1c64f147460e6edfd43db3f8a94b495cdf4
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2024-08-03 20:49:08 +0530

    added Contact with the EmailJS functionality

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,66 @@
+import styles from './ContactStyles.module.css';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+function Contact() {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_e7cqwdh', 'template_cu27mvb', form.current, 'kdoYY7QAyVkY3ZjFM')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send the message, please try again.');
+      });
+
+    e.target.reset(); 
+  };
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required></textarea>
+        </div>
+        <input className="hover btn" type="submit" value="Submit" />
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
