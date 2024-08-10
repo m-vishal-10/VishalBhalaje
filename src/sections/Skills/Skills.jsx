70cfa18a92128823fbafdad496d74c6c994e31e4
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2024-08-11 00:43:46 +0530

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,24 +13,28 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML"/>
-        <SkillList src={checkMarkIcon} skill="CSS"/>
-        <SkillList src={checkMarkIcon} skill="JavaScript"/>
-        <SkillList src={checkMarkIcon} skill="Node"/>
-      </div>
-      <hr />
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React"/>
-        <SkillList src={checkMarkIcon} skill="Angular"/>
-        <SkillList src={checkMarkIcon} skill="TailWind CSS"/>
-      </div>
-      <hr />
-      <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Python"/>
         <SkillList src={checkMarkIcon} skill="Java"/>
         <SkillList src={checkMarkIcon} skill="C"/>
         <SkillList src={checkMarkIcon} skill="C++"/>
       </div>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML"/>
+        <SkillList src={checkMarkIcon} skill="CSS"/>
+        <SkillList src={checkMarkIcon} skill="JavaScript"/>
+        <SkillList src={checkMarkIcon} skill=" Kotlin"/>
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Android Studio"/>
+        <SkillList src={checkMarkIcon} skill="Jetpack Compose"/>
+        <SkillList src={checkMarkIcon} skill="Firebase"/>
+        <SkillList src={checkMarkIcon} skill="React"/>
+        <SkillList src={checkMarkIcon} skill="SpringBoot"/>
+        <SkillList src={checkMarkIcon} skill="Rest API"/>
+      </div>
+      <hr />
+      
       
 
     </section>
